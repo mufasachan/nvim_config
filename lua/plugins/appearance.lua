b122558058c4bdeecd82b67a305608d0f63494a4
mufasachan <mufasachan@hotmail.com>
2023-07-07 19:16:24 +0200

    fix: indent

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -10,14 +10,14 @@ return {
   {
     'nvim-lualine/lualine.nvim',
     dependencies = {'nvim-tree/nvim-web-devicons'},
-    opts = {
-      options = {
-	theme = 'palenight',
-	disabled_filetypes = { 'NvimTree'}
-      },
-      sections = {
-	lualine_x = {'os.date("%H:%M %p")', 'filetype'},
-      },
-    },
-  }
+		opts = {
+			options = {
+				theme = 'palenight',
+				disabled_filetypes = { 'NvimTree'}
+			},
+			sections = {
+				lualine_x = {'os.date("%H:%M %p")', 'filetype'},
+			},
+		},
+	}
 }
