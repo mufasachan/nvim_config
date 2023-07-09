6aefaed2907a8b45856e7a7ecb81b2f550fa28c7
mufasachan <mufasachan@hotmail.com>
2023-07-10 00:01:23 +0200

    Add sessions support

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,14 +1,18 @@
-return {}
--- print(vim.inspect(vim.api.nvim_buf_get_config(vim.fn.buf_getid())))
--- return {
--- 	"folke/persistence.nvim",
--- 	event = "BufReadPre",
--- 	opts = {},
--- 	init = function ()
--- 		vim.api.nvim_create_autocmd('VimEnter', {
--- 			callback = function ()
--- 				require("persistence").load({ last = true })
--- 			end
--- 		})
--- 	end
--- }
+local plugin = {'Shatur/neovim-session-manager'}
+
+plugin.dependencies = 'nvim-lua/plenary.nvim'
+
+function plugin.config()
+	local config = require'session_manager.config'
+	local session_manager = require'session_manager'
+
+	-- There are good autocommand in doc session-manager
+	require'session_manager'.setup{
+		-- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
+		autoload_mode = config.AutoloadMode.CurrentDir,
+		-- Always autosaves session. If true, only autosaves after a session is active.
+		autosave_only_in_session = false,
+	}
+end
+
+return plugin
