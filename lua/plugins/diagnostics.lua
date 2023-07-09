bdd036e61feee793480939150f47bb06e1193aa4
mufasachan <mufasachan@hotmail.com>
2023-07-09 20:07:42 +0200

    Cleaner diagnostics settings

--- lua/plugins/diagnostics.lua
+++ lua/plugins/diagnostics.lua
@@ -1,16 +1,38 @@
-local map = vim.keymap.set
+local plugin = {"folke/trouble.nvim"}
 
-map('n', '<Leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = 'Next'})
-map('n', '<Leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = 'Previous'})
+plugin.dependencies = { "nvim-tree/nvim-web-devicons" }
 
-return {
-	-- Panel
-	{
-		"folke/trouble.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		init = function ()
-			map('n', '<Leader>dw', ':TroubleToggle<CR>', { desc = 'Workspace'})
-			map('n', '<Leader>dd', ':TroubleToggle document_diagnostics<CR>', { desc = 'Document'})
-		end
-	},
-}
+function plugin.config()
+	-- Choose shape of diagnostic icons
+	local function sign(opts)
+		vim.fn.sign_define(opts.name, {
+			texthl = opts.name,
+			text = opts.text,
+			numhl = '',
+		})
+	end
+	sign({name = 'DiagnosticSignError', text = '✘'})
+	sign({name = 'DiagnosticSignWarn', text = '▲'})
+	sign({name = 'DiagnosticSignHint', text = '⚑'})
+	sign({name = 'DiagnosticSignInfo', text = '»'})
+
+	-- Neovim config of diagnostic
+	vim.diagnostic.config({
+		virtual_text = false,
+		severity_sort = true,
+		float = {
+			border = 'rounded',
+			source = 'always',
+		},
+	})
+
+	-- Keymappings for diagnostic
+	local map = vim.keymap.set
+  map('n', '<leader>li', '<cmd>lua vim.diagnostic.open_float()<cr>', {desc = 'Info'})
+	map('n', '<Leader>dn', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = 'Next'})
+	map('n', '<Leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = 'Previous'})
+	map('n', '<Leader>dw', ':TroubleToggle<CR>', { desc = 'Workspace'})
+	map('n', '<Leader>dd', ':TroubleToggle document_diagnostics<CR>', { desc = 'Document'})
+end
+
+return plugin
