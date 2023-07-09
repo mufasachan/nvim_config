8b31b4d326221ccc86268a43fa40f7b612c91419
mufasachan <mufasachan@hotmail.com>
2023-07-10 00:03:31 +0200

    add superb surrounds

---
+++ lua/plugins/surrounds.lua
@@ -0,0 +1,17 @@
+local plugin = {'tpope/vim-surround'}
+
+plugin.keys = {
+  'ds',
+  'cs',
+  'cS',
+  'ys',
+  'yS',
+  'yss',
+  'ySs',
+  'ySS',
+  {'S', mode = 'x'},
+  {'gS', mode = 'x'},
+}
+
+return plugin
+
