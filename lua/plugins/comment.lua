f9ab037b3acc49a70b36728fd6fae5cf98272f12
AwePhD <mathias.reus@gipsa-lab.fr>
2023-10-10 04:44:04 +0200

    comment: add visual keybind

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,16 +5,10 @@ plugin.config = function ()
 	local api = require('Comment.api')
 	local config = require('Comment.config'):get()
 
-	vim.keymap.set('n', '<C-/>', api.toggle.linewise.current)
-	vim.keymap.set('n', '<C-?>', api.toggle.blockwise.current)
+	vim.keymap.set({ 'v', 'n'}, '<C-/>', api.toggle.linewise.current)
+	vim.keymap.set({ 'v', 'n'}, '<C-?>', api.toggle.blockwise.current)
 
 	return config
 end
-
-plugin.opts = {
-toggle={
-	line = 'C-/'
-	}
-}
 
 return plugin
