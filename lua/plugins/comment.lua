c15b1227c7cb9929a25995ff5c50ea6abd2b9e75
AwePhD <mathias.reus@gipsa-lab.fr>
2023-10-10 04:50:06 +0200

    comment: fix visual keybind

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -5,8 +5,20 @@ plugin.config = function ()
 	local api = require('Comment.api')
 	local config = require('Comment.config'):get()
 
-	vim.keymap.set({ 'v', 'n'}, '<C-/>', api.toggle.linewise.current)
-	vim.keymap.set({ 'v', 'n'}, '<C-?>', api.toggle.blockwise.current)
+	vim.keymap.set('n', '<C-/>', api.toggle.linewise.current)
+	vim.keymap.set('n', '<C-?>', api.toggle.blockwise.current)
+	-- Toggle (from help file)
+	local esc = vim.api.nvim_replace_termcodes(
+	'<ESC>', true, false, true
+	)
+	vim.keymap.set('x', '<C-/>', function()
+		vim.api.nvim_feedkeys(esc, 'nx', false)
+		api.toggle.linewise(vim.fn.visualmode())
+	end)
+	vim.keymap.set('x', '<C-?>', function()
+		vim.api.nvim_feedkeys(esc, 'nx', false)
+		api.toggle.blockwise(vim.fn.visualmode())
+	end)
 
 	return config
 end
