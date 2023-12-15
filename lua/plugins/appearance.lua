f7043ec2460adc0a49a020e45417a9c1bb4638c4
mufasachan <mufasachan@hotmail.com>
2023-12-15 22:51:02 +0100

    add theme and better org

--- lua/plugins/appearance.lua
+++ lua/plugins/appearance.lua
@@ -1,6 +1,9 @@
+local function set_theme()
+		vim.cmd("colorscheme catppuccin-macchiato")
+end
+
 return {
-	"sts10/vim-pink-moon",
-	{ "savq/melange-nvim" , config = function ()
-		vim.cmd("colorscheme melange")
-	end},
+	{ "sts10/vim-pink-moon", priority = 1000  },
+	{ "savq/melange-nvim", priority = 1000 },
+	{ "catppuccin/nvim", priority = 1000, config = set_theme }
 }
