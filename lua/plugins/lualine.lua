615275622dff65e01ed8dab3a9876ed929455b77
mufasachan <mufasachan@hotmail.com>
2023-12-15 22:59:16 +0100

    only one luabar, theme auto

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,21 +1,17 @@
-local plugin = {'nvim-lualine/lualine.nvim'}
+local plugin = { 'nvim-lualine/lualine.nvim' }
 
-plugin.dependencies = {'nvim-tree/nvim-web-devicons'}
+plugin.dependencies = { 'nvim-tree/nvim-web-devicons' }
 
 -- SPECIFICS COMPONENTS SETTINGS
 -- path = 1 is relative path
-local filename = {
-	'filename',
-	path = 1,
-}
 plugin.opts = {
 	options = {
-		theme = 'palenight',
-		disabled_filetypes = {'NvimTree'}
+		globalstatus = true,
+		disabled_filetypes = { 'NvimTree' }
 	},
 	sections = {
-    lualine_c = {filename},
-		lualine_x = {'os.date("%H:%M %p")', 'filetype'},
+		lualine_c = { { 'filename', path = 1 }, },
+		lualine_x = { 'os.date("%H:%M %p")', 'filetype' },
 	},
 }
 
