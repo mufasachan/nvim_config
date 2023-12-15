c8a2a2f2771a69c1367bb1cc52b759b6f11e959c
mufasachan <mufasachan@hotmail.com>
2023-12-15 23:53:05 +0100

    add move ts TO

--- lua/plugins/treesitter-textobjects.lua
+++ lua/plugins/treesitter-textobjects.lua
@@ -1,22 +1,43 @@
-local plugin = {'nvim-treesitter/nvim-treesitter-textobjects'}
+local plugin = { 'nvim-treesitter/nvim-treesitter-textobjects' }
 plugin.dependencies = { 'treesitter' }
 
-local opts = {
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true,
-      keymaps = {
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      }
-    },
-  },
-}
 function plugin.config()
-	require("nvim-treesitter.configs").setup{opts}
+	require("nvim-treesitter.configs").setup {
+		textobjects = {
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]c"] = "@conditional.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]C"] = "@conditional.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[c"] = "@conditional.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[C"] = "@conditional.outer",
+				},
+			},
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					['af'] = '@function.outer',
+					['if'] = '@function.inner',
+				}
+			},
+		},
+	}
+end
+
+if false then
+	print("here")
 end
 
 return plugin
