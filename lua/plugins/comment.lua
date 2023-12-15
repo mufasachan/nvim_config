1813e6942adc3540f806faebf8ff76254dc7cd48
mufasachan <mufasachan@hotmail.com>
2023-12-15 18:39:17 +0100

    fix shortcuts for comments

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,26 +1,9 @@
-local plugin = {"numToStr/Comment.nvim"}
+local plugin = { "numToStr/Comment.nvim" }
 plugin.name = "comment"
 
-plugin.config = function ()
-	local api = require('Comment.api')
-	local config = require('Comment.config'):get()
-
-	vim.keymap.set('n', '<C-/>', api.toggle.linewise.current)
-	vim.keymap.set('n', '<C-?>', api.toggle.blockwise.current)
-	-- Toggle (from help file)
-	local esc = vim.api.nvim_replace_termcodes(
-	'<ESC>', true, false, true
-	)
-	vim.keymap.set('x', '<C-/>', function()
-		vim.api.nvim_feedkeys(esc, 'nx', false)
-		api.toggle.linewise(vim.fn.visualmode())
-	end)
-	vim.keymap.set('x', '<C-?>', function()
-		vim.api.nvim_feedkeys(esc, 'nx', false)
-		api.toggle.blockwise(vim.fn.visualmode())
-	end)
-
-	return config
-end
+plugin.opts  = {
+	toggler = { line = '<C-\\>', block = '<C-|>' },
+	opleader = { line = '<C-\\>', block = '<C-|>' },
+}
 
 return plugin
