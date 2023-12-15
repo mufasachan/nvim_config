90c110191d8dfd6168f972a299e55d30b162e132
mufasachan <mufasachan@hotmail.com>
2023-12-16 00:04:25 +0100

    fix goto context

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,11 @@
+local plugin = { 'nvim-treesitter/nvim-treesitter-context' }
+
+plugin.dependencies = { 'treesitter' }
+
+function plugin.config()
+	vim.keymap.set("n", "gc", function()
+		require("treesitter-context").go_to_context()
+	end, { silent = true })
+end
+
+return plugin
