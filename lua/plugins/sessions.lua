9bcba2d05b07dd4dd5002809dd0ac9d044ffa657
AwePhD <mathias.reus@gipsa-lab.fr>
2024-03-28 04:45:09 +0100

    feat(sessions): replace sessions plugins

--- lua/plugins/sessions.lua
+++ lua/plugins/sessions.lua
@@ -1,19 +1,15 @@
-local plugin = {'Shatur/neovim-session-manager'}
+local plugin = { "olimorris/persisted.nvim" }
+plugin.name = "persisted"
 
-plugin.dependencies = 'nvim-lua/plenary.nvim'
+plugin.dependencies = { "telescope" }
 
-function plugin.config()
-	local config = require'session_manager.config'
+plugin.config = function()
+	local telescope = require "telescope"
+	telescope.load_extension('persisted')
 
-	vim.keymap.set('n', '<Leader>s', ':SessionManager load_session<CR>', {desc = 'Sessions'})
+	vim.keymap.set("n", "<Leader>s", "<CMD>Telescope persisted<CR>", { desc = "Sessions", silent = true })
 
-	-- There are good autocommand in doc session-manager
-	require'session_manager'.setup{
-		-- Define what to do when Neovim is started without arguments. Possible values: Disabled, CurrentDir, LastSession
-		autoload_mode = config.AutoloadMode.CurrentDir,
-		-- Always autosaves session. If true, only autosaves after a session is active.
-		autosave_only_in_session = false,
-	}
+	require "persisted".setup { autoload = true }
 end
 
 return plugin
