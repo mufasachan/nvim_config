53295fbd26b240063a9c8d7f3cd84be6c1b785ca
AwePhD <mathias.reus@gipsa-lab.fr>
2024-07-16 00:26:55 +0200

    update + keybind out normal

--- lua/plugins/cmdline.lua
+++ lua/plugins/cmdline.lua
@@ -5,10 +5,16 @@ plugin.dependencies = {
 	"MunifTanjim/nui.nvim"
 }
 
-plugin.config = function()
-	require("fine-cmdline").setup()
-
-	vim.api.nvim_set_keymap("n", "<CR>", "<cmd>FineCmdline<CR>", { noremap = true })
+local opts = {
+	hooks = {
+		after_mount = function(input)
+			vim.keymap.set("n", "q", "<CMD>q<CR>", { buffer = input.bufnr })
+		end
+	}
+}
+plugin.config = function ()
+	require("fine-cmdline").setup(opts)
+	vim.keymap.set("n", "<CR>", "<CMD>FineCmdline<CR>")
 end
 
 return plugin
