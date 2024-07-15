5c72d36b1072b667ca71cf4e017b5886c94b98b6
AwePhD <mathias.reus@gipsa-lab.fr>
2024-07-15 21:47:06 +0200

    update wishlist

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
 -- Might add in the future:
 -- m4xshen/hardtime.nvim: Better habits
 -- ThePrimeagen/vim-be-good: Game to be better at nvim
+-- TODOs:
+-- - Change color of the cursor when there is a code action.
+-- 		See https://github.com/neovim/nvim-lspconfig/wiki/Code-Actions
+-- - Set python configuration with tools in the local env (not **Mason**)
+-- - Add project feature
+-- 		- Proper workspace listing that can be resumed
+-- 		- Sort of welcome pane when opening neovim with no argument.
+
 require("user.autocommands")
 require("user.ft")
 require("user.keymappings")
