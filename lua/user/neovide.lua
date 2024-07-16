9fd52e02326118f6d44d76aaa122fa49692cac10
AwePhD <mathias.reus@gipsa-lab.fr>
2024-07-16 03:14:22 +0200

    chore(gui): minor changes

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -5,9 +5,10 @@ local function neovide_setup()
 	end
 	vim.keymap.set("n", "<C-F11>", _toggle_fullscreen)
 
-	vim.g.neovide_cursor_vfx_mode = "pixiedust"
+	vim.g.neovide_cursor_vfx_mode = ""
 	vim.o.guifont = "FiraCode Nerd Font:h16"
-	vim.g.neovide_transparency = 0.85
+	vim.g.neovide_transparency = 0.9
+	vim.g.neovide_cursor_animation_length = 0.1
 
 	vim.api.nvim_set_keymap("i", "<SC-V>", "<C-R>+", { noremap = true })
 end
