83d729ab58b8dd57f3992db334067bacbf9684f0
AwePhD <mathias.reus@gipsa-lab.fr>
2025-01-30 20:54:51 +0100

    feat: add keybinds

--- ftplugin/norg.lua
+++ ftplugin/norg.lua
@@ -1,12 +1,19 @@
 vim.wo.conceallevel = 3
+
+-- Global shortcut
 local wk = require("which-key")
 wk.add({
 	{ lhs = "<leader>N",  group = "Neorg",                        buffer = true },
 	{ lhs = "<leader>Nn", rhs = "<Plug>(neorg.dirman.new-note)", desc = "New note" },
-	{ lhs = "<leader>Nf", rhs = "<Plug>(neorg.telescope.find_norg_files)", desc = "Find files" },
+	{ lhs = "<leader>Ns",  group = "Search",                        buffer = true },
+	{ lhs = "<leader>Nc", rhs = "<CMD>Neorg toggle-concealer<CR>", desc = "Toggle concealer" },
+	{ lhs = "<leader>Nsf", rhs = "<Plug>(neorg.telescope.find_norg_files)", desc = "Find files" },
+	{ lhs = "<leader>Nsh", rhs = "<Plug>(neorg.telescope.search_headings)", desc = "Search headings" },
 	{ lhs = "<C-l>", rhs = "<CMD>Telescope neorg insert_file_link<CR>", mode = "i", desc = "Insert link" },
 })
 
+
+-- Keyboard shortcut with snippet for insert mode.
 local ls = require("luasnip")
 local s = ls.snippet
 local t = ls.text_node
