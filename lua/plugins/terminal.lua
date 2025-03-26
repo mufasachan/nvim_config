327d868256bc486df3448176342e35e2142a2d03
muffie <mufasachan@hotmail.com>
2025-03-26 18:55:43 +0100

    feat: page{up,down} for terminal navigation

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -8,6 +8,9 @@ function plugin.init()
 	map("n", "<M-2>", ":2ToggleTerm direction=float<CR>")
 
 	function _G.set_terminal_keymaps()
+		map("t", "<C-Tab>", [[<C-\><C-n>]])
+		map("t", "<C-PageUp>", "<CMD>tabp<CR>")
+		map("t", "<C-PageDown>", "<CMD>tabn<CR>")
 		map("t", "jk", [[<C-\><C-n>]])
 		-- On my tablet, I only use one orientation.
 		map("t", "<C-k>", "<Cmd>wincmd k<CR>")
