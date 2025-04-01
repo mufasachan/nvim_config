aae7cd163f429d4eb343684a3eaaeb5ec0299632
AwePhD <mathias.reus@gipsa-lab.fr>
2025-04-01 17:19:17 +0200

    fix: latexmk is used, not latexrun

--- lua/plugins/languages/latex.lua
+++ lua/plugins/languages/latex.lua
@@ -1,13 +1,15 @@
 local vimtex = { "lervag/vimtex" }
 function vimtex.init()
-	vim.g.vimtex_view_general_viewer = "sioyek"
-	vim.g.vimtex_quickfix_mode = 0
-	vim.g.vimtex_compiler_latexrun_engines = {
-		_ = "xelatex",
-		pdflatex = "pdflatex",
-		lualatex = "lualatex",
-		xelatex = "xelatex",
-	}
+  vim.g.vimtex_view_general_viewer = "sioyek"
+  vim.g.vimtex_quickfix_mode = 0
+  vim.g.vimtex_compiler_latexmk = {
+    aux_dir = "aux",
+    out_dir = "out",
+  }
+  vim.g.vimtex_compiler_latexmk_engines = {
+    -- Doc say context + luatex
+    _ = "-lualatex"
+  }
 end
 
 local plugins = { vimtex, "barreiroleo/ltex_extra.nvim" }
