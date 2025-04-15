76deac640718b8e63e2799544f1715f24b4e207f
muffie <mufasachan@hotmail.com>
2025-04-15 15:56:30 +0200

    fix: wrong setup TS???

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,33 +1,42 @@
+---@diagnostic disable: missing-fields
 local plugin = { "nvim-treesitter/nvim-treesitter" }
 plugin.name = "treesitter"
 
 plugin.build = {
-	build = ":TSUpdate",
+  build = ":TSUpdate",
 }
 
--- See :help nvim-treesitter-modules
-plugin.opts = {
-	ensure_installed = {
-		"c",
-		"vim",
-		"vimdoc",
-		"query",
-		"markdown",
-		"markdown_inline",
-		"bash",
-		"json",
-		"python",
-		"yaml",
-		"lua"
-	},
-	ignore_install = { "latex" },
-	highlight = { enable = true },
-	incremental_selection = { enable = true },
-	textobjects = { enable = true },
-	indent = {
-		enable = true,
-		disable = { "dart" },
-	},
-}
+function plugin.init()
+  require "nvim-treesitter.configs".setup {
+    ensure_installed = {
+      "c",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "bash",
+      "json",
+      "python",
+      "yaml",
+      "lua"
+    },
+    ignore_install = { "latex" },
+
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- Automatically install missing parsers when entering buffer
+    auto_install = false,
+
+    highlight = { enable = true },
+    incremental_selection = { enable = true },
+    textobjects = { enable = true },
+    indent = {
+      enable = true,
+      disable = { "dart" },
+    },
+    additional_vim_regex_highlighting = true,
+  }
+end
 
 return plugin
