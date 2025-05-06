3126f8a5b53497eb5812dc0cde7173a248668ecf
muffie <mufasachan@hotmail.com>
2025-05-06 09:51:44 +0200

    feat: add refactor plugin default CA

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,7 +1,19 @@
 local M = { "nvimtools/none-ls.nvim" }
 
-M.dependencies = { "plenary" }
+M.dependencies = {
+  "plenary",
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = { "plenary", "treesitter" },
+    opts = {}
+  }
+}
 
-M.opts = {}
+function M.config()
+  local null_ls = require("null-ls")
+  null_ls.setup {
+    sources = { null_ls.builtins.code_actions.refactoring }
+  }
+end
 
 return M
