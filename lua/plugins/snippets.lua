3e63a0d64286e41d881096af249e4a729cd76fed
AwePhD <mathias.reus@gipsa-lab.fr>
2025-05-20 19:28:35 +0200

    fix: import friendly snippet from std_runtime

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -3,6 +3,7 @@ M.depencies = { "rafamadriz/friendly-snippets" }
 M.version = "v2.*"
 M.build = "make install_jsregexp"
 function M.config()
+  require("luasnip.loaders.from_vscode").lazy_load()
   require("luasnip.loaders.from_lua").load {
     paths = { "./lua/snippets" }
   }
