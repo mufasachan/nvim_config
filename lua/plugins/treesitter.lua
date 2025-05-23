a3c762ce88f0ae7645010b14f668e8f1e12b442e
AwePhD <mathias.reus@gipsa-lab.fr>
2025-05-23 08:08:25 +0200

    feat: swap parameters!

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -56,6 +56,15 @@ M.opts = {
         ["[C"] = "@conditional.outer",
       },
     },
+    swap = {
+      enable = true,
+      swap_next = {
+        ["<leader>a"] = "@parameter.inner",
+      },
+      swap_previous = {
+        ["<leader>A"] = "@parameter.inner",
+      },
+    },
     select = {
       enable = true,
       lookahead = true,
