64628e4efb9c1344750e3a9167bb112d25c4227e
muffie <mufasachan@hotmail.com>
2025-05-31 12:31:36 +0200

    feat: gS for dynamic symbol search

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,3 +1,15 @@
+-- wait https://github.com/neovim/neovim/issues/33577 for better on_attach modification.
+-- seems to wait 0.12 https://github.com/neovim/neovim/issues/32074
+local wk = require "which-key"
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(event)
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+    if client and client.name == "basedpyright" then
+      wk.add { { "gS", "<CMD>:Telescope lsp_dynamic_workspace_symbols<CR>", silent = true, buffer = event.buf } }
+    end
+  end
+})
 return {
   settings = {
     basedpyright = {
