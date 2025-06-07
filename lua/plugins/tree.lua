5bb7a273b8411fe80e654fe597fbb38dbf24da51
muffie <mufasachan@hotmail.com>
2025-06-07 12:17:58 +0200

    chore: clean wk <Leader>
    Less clutter for Leader which key. and tree exploro is a bit cleaned

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,60 +1,60 @@
-local plugin = { "nvim-tree/nvim-tree.lua" }
-plugin.name = "nvim-tree"
-plugin.dependencies = { "nvim-web-devicons", "which-key" }
+local M = { "nvim-tree/nvim-tree.lua" }
+M.name = "nvim-tree"
+M.dependencies = { "nvim-web-devicons", "which-key" }
 
-local function on_attach_tree(bufnr)
-	-- Set nvim-tree maps
-	local api = require "nvim-tree.api"
-	api.config.mappings.default_on_attach(bufnr)
+local function on_attach(bufnr)
+  -- Set nvim-tree maps
+  local api = require "nvim-tree.api"
+  api.config.mappings.default_on_attach(bufnr)
 
-	local del = function(lhs) vim.keymap.del("n", lhs, { buffer = bufnr }) end
-	del("<2-LeftMouse>")
-	del("<2-RightMouse>")
-	del("y")
-	del("<")
-	del(">")
-	del("W")
-	del("<C-R>")
-	del("<BS>")
-	del("e")
-	del("r")
-	del("<Tab>")
+  local del = function(lhs) vim.keymap.del("n", lhs, { buffer = bufnr }) end
+  del("<2-LeftMouse>")
+  del("<2-RightMouse>")
+  del("y")
+  del("<")
+  del(">")
+  del("W")
+  del("<C-R>")
+  del("<BS>")
+  del("e")
+  del("r")
+  del("<Tab>")
 
-	local map = function(lhs, rhslua, desc)
-		vim.keymap.set("n", lhs, rhslua, {
-			desc = "nvim-tree: " .. desc,
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			nowait = true
-		})
-	end
-	map("l", api.node.open.edit, "Open")
-	map("e", api.node.open.edit, "Open")
-	map("rr", api.fs.rename, "Rename")
-	map("y", api.fs.copy.node, "Copy file")
-	map("re", api.fs.rename_basename, "Rename basename")
-	map("ro", api.fs.rename_sub, "Rename omit filename")
+  local map = function(lhs, rhslua, desc)
+    vim.keymap.set("n", lhs, rhslua, {
+      desc = "nvim-tree: " .. desc,
+      buffer = bufnr,
+      noremap = true,
+      silent = true,
+      nowait = true
+    })
+  end
+  map("l", api.node.open.edit, "Open")
+  map("e", api.node.open.edit, "Open")
+  map("rr", api.fs.rename, "Rename")
+  map("y", api.fs.copy.node, "Copy file")
+  map("re", api.fs.rename_basename, "Rename basename")
+  map("ro", api.fs.rename_sub, "Rename omit filename")
 end
 
-plugin.opts = {
-	filters = { dotfiles = true },
-	on_attach = on_attach_tree,
-	renderer = { group_empty = true },
-	view = { side = 'right', width = 30 },
-	sync_root_with_cwd = true,
-	disable_netrw = true, -- netrw-noload
-	hijack_directories = { enable = false }, -- for auto-sessions
+M.opts = {
+  filters = { dotfiles = true },
+  on_attach = on_attach,
+  renderer = { group_empty = true },
+  view = { side = "right", width = 30 },
+  sync_root_with_cwd = true,
+  disable_netrw = true,                    -- netrw-noload
+  hijack_directories = { enable = false }, -- for auto-sessions
 }
 
-function plugin.config(_, opts)
-	local api = require("nvim-tree.api")
-	require("which-key").add({
-		{ "<Leader>e", api.tree.toggle, desc = "Explorer", silent = true },
-    { "<Leader>E", function () api.tree.toggle({find_file=true}) end, desc = "Explorer", silent = true },
-	})
+function M.config(_, opts)
+  local api = require("nvim-tree.api")
+  require("which-key").add {
+    { "<Leader>e", api.tree.toggle,                                      desc = "Explorer", silent = true, hidden = true },
+    { "<Leader>E", function() api.tree.toggle({ find_file = true }) end, desc = "Explorer", silent = true, hidden = true },
+  }
 
-	require("nvim-tree").setup(opts)
+  require("nvim-tree").setup(opts)
 end
 
-return plugin
+return M
--- lua/user/keymappings.lua
+++ lua/user/keymappings.lua
@@ -1,55 +1,31 @@
--- Shortcuts
-vim.keymap.set({ "n", "x", "o" }, "<M-h>", "^", { desc = "Start of line" })
-vim.keymap.set({ "n", "x", "o" }, "<M-l>", "g_", { desc = "End of line" })
+local wk = require("which-key")
 
--- Clear current line
-vim.keymap.set("n", "\\c", "0D", { silent = true })
-
--- Basic clipboard interaction
--- disable on tablet
--- vim.keymap.set({'n', 'x'}, 'gy', '"+y') -- copy
--- vim.keymap.set({'n', 'x'}, 'gp', '"+p') -- paste
-
--- Delete text
-vim.keymap.set({ "n", "x" }, "x", '"_x')
-
--- Exit insert mode
-vim.keymap.set("i", "jk", "<esc>")
-
--- better up/down
-vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
-vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-
--- Splits
-vim.keymap.set("n", "<C-z>", "<CMD>vsplit<CR>", { silent = true })
-vim.keymap.set("n", "<C-S-z>", "<CMD>split<CR>", { silent = true })
-vim.keymap.set("n", "<C-h>", "<C-w>h", { remap = true })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { remap = true })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { remap = true })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { remap = true })
-vim.keymap.set("n", "<C-Left>", "<C-w>h", { remap = true })
-vim.keymap.set("n", "<C-Down>", "<C-w>j", { remap = true })
-vim.keymap.set("n", "<C-Up>", "<C-w>k", { remap = true })
-vim.keymap.set("n", "<C-Right>", "<C-w>l", { remap = true })
-
--- Move Lines
-vim.keymap.set("n", "<A-j>", "<cmd>m .+1<cr>==", { silent = true })
-vim.keymap.set("n", "<A-k>", "<cmd>m .-2<cr>==", { silent = true })
-vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { silent = true })
-vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { silent = true })
-vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { silent = true })
-vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { silent = true })
-
--- Commands
-vim.keymap.set("n", "<Leader>q", ":q<CR>", { desc = "Quit" })
-vim.keymap.set("n", "<Leader>Q", ":qa<CR>", { desc = "Quit all" })
-vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Save" })
--- Commands: Help page
-vim.keymap.set("x", "H", '"xy:help <C-R>x<CR>',
-  { desc = "Back", silent = true, noremap = true })
-
--- Draft
-local wk = require "which-key"
 wk.add {
-  { "<Leader>D", "<CMD>enew<CR>", desc = "Create draft", silent = true }
+  -- Common
+  { "<Leader>q", "<CMD>q<CR>",       desc = "Quit",               hidden = true },
+  { "<Leader>w", "<CMD>w<CR>",       desc = "Save",               silent = true, hidden = true },
+  { "<Leader>D", "<CMD>enew<CR>",    desc = "Create draft",       silent = true },
+  -- No register for x
+  { mode = "nx", lhs = "x",          rhs = '"_x' },
+  -- jk is Esc
+  { mode = "i",  lhs = "jk",         rhs = "<ESC>" },
+  -- better go/up
+  { mode = "nx", "j",                "v:count == 0 ? 'gj' : 'j'", silent = true, expr = true },
+  { mode = "nx", "k",                "v:count == 0 ? 'gk' : 'k'", silent = true, expr = true },
+  -- Split navigation
+  { "<C-h>",     "<C-w>h",           remap = true },
+  { "<C-j>",     "<C-w>j",           remap = true },
+  { "<C-k>",     "<C-w>k",           remap = true },
+  { "<C-l>",     "<C-w>l",           remap = true },
+  { "<C-Left>",  "<C-w>h",           remap = true },
+  { "<C-Down>",  "<C-w>j",           remap = true },
+  { "<C-Up>",    "<C-w>k",           remap = true },
+  { "<C-Right>", "<C-w>l",           remap = true },
+  { "<A-j>",     "<CMD>m .+1<CR>==", silent = true },
+  { "<A-k>",     "<CMD>m .-2<CR>==", silent = true },
+  -- Move lines
+  { mode = "i",  "<A-j>",            "<ESC><CMD>m .+1<CR>==",     silent = true },
+  { mode = "i",  "<A-k>",            "<ESC><CMD>m .-2<CR>==",     silent = true },
+  { mode = "v",  "<A-j>",            ":m '>+1<cr>gv=gv",          silent = true },
+  { mode = "v",  "<A-k>",            ":m '<-2<cr>gv=gv",          silent = true },
 }
