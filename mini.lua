1ab2fefe60331bccc30b31b89bdf180652289db4
AwePhD <mathias.reus@gipsa-lab.fr>
2025-09-08 14:26:43 +0200

    feat: mini more keybinds

--- mini.lua
+++ mini.lua
@@ -3,8 +3,13 @@ vim.o.nu, vim.o.rnu = true, true
 vim.o.shm = vim.o.shm .. "I"
 
 vim.g.mapleader = " "
+vim.api.nvim_set_keymap("n", "<Leader>w", "<CMD>w<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>n", "<CMD>next<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>p", "<CMD>prev<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>b", "<CMD>b#<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>e", "<CMD>enew<CR>", { silent = true })
 vim.api.nvim_set_keymap("n", "<Leader>q", "<CMD>qa!<CR>", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Left>", "<C-w>h", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Down>", "<C-w>j", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Up>", "<C-w>k", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Right>", "<C-w>l", { silent = true })
