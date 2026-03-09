074aab1f1b7fb03d692d096539fc2f9aae868091
mufasachan <mufasachan@hotmail.com>
2026-03-09 14:53:55 +0100

    fix(typst): open new windown preview

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -5,5 +5,8 @@ return {
   opts = {
     -- mason tinymist
     dependencies_bin = { tinymist = "tinymist" },
+    -- else no open window opens :( + mute output
+    open_cmd = "firefox --new-window %s 2>&1 > /dev/null",
+    debug = true,
   },
 }
