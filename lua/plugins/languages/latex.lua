366696f699f4d91aefcb5a37ace83c314209c6c0
muffie <mufasachan@hotmail.com>
2026-03-13 11:39:37 +0100

    feat(tex): require local .latexmk for option

--- lua/plugins/languages/latex.lua
+++ lua/plugins/languages/latex.lua
@@ -2,10 +2,6 @@ local vimtex = { "lervag/vimtex" }
 function vimtex.init()
   vim.g.vimtex_view_general_viewer = "sioyek"
   vim.g.vimtex_quickfix_mode = 0
-  vim.g.vimtex_compiler_latexmk = {
-    aux_dir = "aux",
-    out_dir = "out",
-  }
   vim.g.vimtex_compiler_latexmk_engines = {
     -- Doc say context + luatex
     _ = "-lualatex"
