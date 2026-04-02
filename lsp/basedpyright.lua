935417e23c1102e16f83881d7103631ddf2c69f2
mufasachan <mufasachan@hotmail.com>
2026-04-02 17:34:39 +0200

    feat(lsp): go to import in python
    go to declarations still goes to original function definition. This one
    ensures to go to the import line in the file `gI`.

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -5,11 +5,47 @@ vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(event)
     local client = vim.lsp.get_client_by_id(event.data.client_id)
 
+    local buf = event.buf
     if client and client.name == "basedpyright" then
-      wk.add { { "gS", "<CMD>:FzfLua lsp_live_workspace_symbols<CR>", silent = true, buffer = event.buf } }
+      wk.add { { "gS", "<CMD>:FzfLua lsp_live_workspace_symbols<CR>", silent = true, buffer = buf } }
     end
+
+    wk.add({
+      {
+        "gI",
+        function()
+          -- Search forward from top of file for an import line with this symbol
+          local word = vim.fn.expand("<cword>")
+          local saved_position = vim.api.nvim_win_get_cursor(0)
+          local saved_search = vim.fn.getreg("/")
+
+          vim.api.nvim_win_set_cursor(0, { 1, 0 })
+          local found = vim.fn.search([[\(import\|as\).*\<]] .. word .. [[\>]], "Ws")
+
+          vim.fn.setreg("/", saved_search)
+          vim.cmd("nohlsearch")
+
+          -- Restore position if nothing found
+          if found == 0 then
+            vim.api.nvim_win_set_cursor(0, saved_position)
+            vim.notify("No import found for: " .. word, vim.log.levels.WARN)
+          else
+            local import_position = vim.api.nvim_win_get_cursor(0)
+
+            -- move cursor to the searched world
+            local column_import = vim.api.nvim_get_current_line():find(vim.pesc(word))
+            vim.api.nvim_win_set_cursor(0, saved_position)
+            vim.cmd("normal! m'")
+            vim.api.nvim_win_set_cursor(0, { import_position[1], column_import - 1 })
+          end
+        end,
+        buffer = buf,
+        desc = "Go to import line"
+      }
+    })
   end
 })
+
 return {
   settings = {
     basedpyright = {
