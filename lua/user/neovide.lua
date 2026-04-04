308bd73f2bb62f21596da84eebace8a5cae9daf8
muffie <mufasachan@hotmail.com>
2026-04-04 14:38:31 +0200

    fix(neovide): opacity and simplify
    before it was transparency, now it's opacity

--- lua/user/neovide.lua
+++ lua/user/neovide.lua
@@ -1,18 +1,14 @@
-local function neovide_setup()
-	vim.g.neovide_fullscreen = true
-	local function _toggle_fullscreen()
-		vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
-	end
-	vim.keymap.set("n", "<C-F11>", _toggle_fullscreen)
-
-	vim.g.neovide_cursor_vfx_mode = ""
-	vim.o.guifont = "GeistMono Nerd Font Mono:h18"
-	vim.g.neovide_transparency = 0.9
-	vim.g.neovide_cursor_animation_length = 0.1
-
-	vim.api.nvim_set_keymap("i", "<SC-V>", "<C-R>+", { noremap = true })
-end
-
 if vim.g.neovide then
-	neovide_setup()
+  vim.g.neovide_fullscreen = true
+  local function _toggle_fullscreen()
+    vim.g.neovide_fullscreen = not vim.g.neovide_fullscreen
+  end
+  vim.keymap.set("n", "<C-F11>", _toggle_fullscreen)
+
+  vim.g.neovide_cursor_vfx_mode = ""
+  vim.o.guifont = "GeistMono Nerd Font Mono:h18"
+  vim.g.neovide_opacity = 0.9
+  vim.g.neovide_cursor_animation_length = 0
+
+  vim.api.nvim_set_keymap("i", "<SC-V>", "<C-R>+", { noremap = true })
 end
